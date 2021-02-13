45b962f8a8e480d4c609b6c390c3326945d03859
Pavel Erikov <prosto.pasha123@gmail.com>
2021-02-13 17:25:38 +0300

    Change Lab1.java for testing Point3d, added comments

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,11 +1,51 @@
+import java.util.Scanner;
 public class Lab1 {
     public static void main(String[] args){
-        Point2d test = new Point2d(1, 2);
-        Point2d test1 = new Point2d(2, 1);
-        Point2d test2 = new Point2d(1, 2);
-        System.out.println(test == test1);
-        System.out.println(test == test2);
-        System.out.println(test.equals(test1));
-        System.out.println(test.equals(test2));
+        //Вводим координаты трех точек
+        Scanner in = new Scanner(System.in);
+        Point3d point1 = getPoint3d(in);
+        Point3d point2 = getPoint3d(in);
+        Point3d point3 = getPoint3d(in);
+        //Проверка на совпадение
+        if(point1.equals(point2) || point1.equals(point3) ||
+           point2.equals(point3)) {
+            System.out.print("Одна из точек равна другой! " +
+                    "Ошибка рассчета площади.");
+            return;
+        }
+        //Выводим введенные точки на консоль
+        printPoint3d(point1);
+        printPoint3d(point2);
+        printPoint3d(point3);
+        //Рассчитываем площадь треугольника
+        double area = computeArea(point1, point2, point3);
+        //Выводим в консоль площадь с точность до
+        //двух знаков после запятой
+        System.out.printf("Площадь треугольника: %.2f", area);
+    }
+    //Функция расчета площади треугольника по координатам вершин
+    //Принимает три аргумента типа Point3d
+    //Возвращает число с плавающей точкой типа double
+    public static double computeArea(Point3d a, Point3d b, Point3d c){
+        double ab = a.distanceTo(b);
+        double ac = a.distanceTo(c);
+        double bc = b.distanceTo(c);
+        double p = (ab + ac + bc) / 2.0;
+        return Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+    }
+    //Функция ввода 3-х координаты для точки Point3d
+    //Возвращает экземпляр объекта типа Point3d
+    //Принимает сканнер для ввода с консоли
+    public static Point3d getPoint3d(Scanner in){
+        System.out.print("Введите 3 координаты: ");
+        double x, y, z;
+        x = in.nextDouble(); y = in.nextDouble(); z = in.nextDouble();
+        return new Point3d(x, y, z);
+    }
+    //Функция вывода объекта типа Point3d в консоль
+    public static void printPoint3d(Point3d point){
+        System.out.print("x: " + point.getX());
+        System.out.print(" y: " + point.getY());
+        System.out.println(" z: " + point.getZ());
     }
 }
--- src/Point3d.java
+++ src/Point3d.java
@@ -1,24 +1,33 @@
 public class Point3d {
+    //Координаты x, y, z
     private double _x;
     private double _y;
     private double _z;
+    //Конструктор инициализации
     public Point3d(double x, double y, double z){
         _x = x;
         _y = y;
         _z = z;
     }
+    //Конструктор по умолчанию
     public Point3d() { this(0,0,0); }
+    //Получение каждой координаты
     public double getX() { return _x; }
     public double getY() { return _y; }
     public double getZ() { return _z; }
+    //Установка каждой координаты
     public void setX(double x) { _x = x; }
     public void setY(double y) { _y = y; }
     public void setZ(double z) { _z = z; }
+    //Функция сравнения двух точек по их координатам
     public boolean equals(Point3d other){
         return _x == other._x &&
                _y == other._y &&
                _z == other._z;
     }
+    //Функция рассчета расстояния между точками
+    //Принимает аргумент типа Point3d
+    //Возвращает растояние между точкой this и target
     public double distanceTo(Point3d target){
         double res = Math.sqrt(Math.pow(target._x - _x, 2) +
                 Math.pow(target._y - _y, 2) +
