01154663bb8e8f0523befc315c9a60d1f6667e9e
Pavel Erikov <prosto.pasha123@gmail.com>
2021-02-20 21:51:04 +0300

    Change URLDepthPair and Crawler class

--- src/Crawler.java
+++ src/Crawler.java
@@ -4,71 +4,71 @@ import java.util.LinkedList;
 import java.lang.*;
 
 public class Crawler {
-    private final String OPEN_TAG = "<a";
-    private final String CLOSE_TAG = "</a>";
-    private final String PAR_HREF = "href=\"";
-    private final String URL_PREFIX = "http://";
+    private static final String OPEN_TAG = "<a ";
+    private static final String CLOSE_TAG = "</a>";
+    private static final String PAR_HREF = "href=\"";
+    private static final String URL_PREFIX = "http://";
 
-    public LinkedList<URLDepthPair> visited;
-    public int max_depth = 4;
+    private LinkedList<URLDepthPair> visited;
 
-    public Crawler(){
-        visited = new LinkedList<>();
-    }
+    public Crawler(){ visited = new LinkedList<>(); }
 
-    public boolean isValidHref(String href){
-        for(int i = 0; i < visited.size(); i++)
-            if(visited.get(i).getUrl().equals(href))
-                return false;
-        return true;
-    }
-
-    public void getSites(String site, int depth) throws IOException {
-        if(depth >= max_depth) return;
-        URL url = new URL(site);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-        StringBuffer page = new StringBuffer();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            //TODO: сразу анализировать ссылки
-            page.append(line);
-        }
-
-        Integer open = page.indexOf(OPEN_TAG), close = 0;
+    //Функция принимает веб-страницу, глубину и максимальную глубину
+    //Функция анализирует веб-страницу, отмечая найденные ссылки
+    //visited и вызывая для них функцию getSites
+    private void goToValidSites(StringBuffer page, int depth, int max_depth) {
+        int open = page.indexOf(OPEN_TAG), close = 0;
         String link = "";
-        while(open != -1){
+        while (open != -1) {
             open = page.indexOf(PAR_HREF, open);
             close = page.indexOf("\"", open + PAR_HREF.length());
-            if(open == -1 || close == -1 || page.indexOf(CLOSE_TAG, close) == -1)
+            if (open == -1 || close == -1 || page.indexOf(CLOSE_TAG, close) == -1)
                 break;
             link = page.substring(open + PAR_HREF.length(), close);
-            if(link.startsWith(URL_PREFIX))
-                if(isValidHref(link)) {
+            if (link.startsWith(URL_PREFIX))
+                if (!visited.contains(new URLDepthPair(link, 1))) {
                     visited.add(new URLDepthPair(link, depth));
-                    getSites(link, depth + 1);
+                    getSites(link, depth + 1, max_depth);
+                    //Вывод сделан для того, чтобы можно было наблюдать за процессом
+                    System.out.println("[URL: " + link + ", Depth: " + depth + "]");
                 }
             open = page.indexOf(OPEN_TAG, close);
         }
-
-        reader.close();
     }
 
-    public static void print_list(LinkedList<URLDepthPair> list){
-        if(list == null || list.size() == 0)
-            throw new IllegalArgumentException("List cannot be null or empty");
-        for(int i = 0; i < list.size() - 1; i++)
-            System.out.println(list.get(i) + ", ");
-        System.out.print(list.get(list.size() - 1));
+    //Функция принимает url сайта, глубину и максимальную глубину
+    //Вспомогательная рекурсивная функция, которая получает веб-страницу
+    //по url и вызывает функцию goToValidSites для её анализа
+    private void getSites(String site, int depth, int max_depth) {
+        try {
+            if (depth >= max_depth) return;
+            URL url = new URL(site);
+            URLConnection conn = url.openConnection();
+            conn.setReadTimeout(10000);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            StringBuffer page = new StringBuffer();
+
+            while ((line = reader.readLine()) != null)
+                page.append(line);
+            reader.close();
+
+            goToValidSites(page, depth, max_depth);
+        }
+        catch (Exception e) { }
     }
 
-    public static void main(String[] args) throws IOException {
-        Crawler crawler = new Crawler();
-        int depth = 0;
-        String site = "http://bio.acousti.ca";
-        LinkedList<URLDepthPair> list = new LinkedList<>();
-        crawler.getSites(site, depth + 1);
-        list.add(new URLDepthPair(site, depth));
-
-        print_list(crawler.visited);
+    //Функция принимает ссылку на исходный сайт и максимальную глубину поиска
+    //Возвращает список посещенных сайтов
+    public LinkedList<URLDepthPair> getSites(String site, int max_depth) {
+        if(max_depth < 1)
+            throw new IllegalArgumentException("Depth cannot be less than 1");
+        if(!site.startsWith(URL_PREFIX))
+            throw new IllegalArgumentException("Url cannot starts with not http://");
+        visited.clear();
+        visited.add(new URLDepthPair(site, 0));
+        System.out.println("[URL: " + site + ", Depth: " + 0 + "]");
+        getSites(site, 1, max_depth);
+        return visited;
     }
 }
--- src/URLDepthPair.java
+++ src/URLDepthPair.java
@@ -1,21 +1,33 @@
 public class URLDepthPair {
+    private final String URL_PREFIX = "http://";
     private String Url;
     private int Depth;
 
     public URLDepthPair(String url, int depth){
-        Url = url;
-        Depth = depth;
+        setUrl(url);
+        setDepth(depth);
     }
 
     public String getUrl() { return Url; }
     public int getDepth() { return Depth; }
+
+    public void setUrl(String url) {
+        if(!url.startsWith(URL_PREFIX))
+            throw new IllegalArgumentException("URL cannot starts with URL_PREFIX");
+        Url = url;
+    }
+    public void setDepth(int depth) {
+        if(depth < 0)
+            throw new IllegalArgumentException("Depth cannot be less than 0");
+        Depth = depth;
+    }
 
     public boolean equals(Object obj){
         if(obj == this) return true;
         if(obj == null || obj.getClass() != this.getClass())
             return false;
         URLDepthPair other = (URLDepthPair) obj;
-        return Url.equals(other.Url) && Depth == other.Depth;
+        return Url.equals(other.Url);// && Depth == other.Depth;
     }
     public int hashCode(){
         return 31 * Url.hashCode() + Depth;
