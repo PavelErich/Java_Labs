dca377a0125a6270d1926c9b117721cf7335b358
Pavel Erikov <prosto.pasha123@gmail.com>
2021-03-27 21:53:05 +0300

    Added multi-threading, change Crawler class and App class

--- src/App.java
+++ src/App.java
@@ -2,21 +2,23 @@ import java.util.HashSet;
 
 public class App {
     public static void main(String[] args) {
-        if(args.length < 2)
-            throw new IllegalArgumentException("Count args cannot be less 2");
+        //if(args.length < 2)
+        //    throw new IllegalArgumentException("Count args cannot be less 2");
         //Сайт для тестирования: "http://bio.acousti.ca";
-        String site = args[0];
-        int max_depth = Integer.parseInt(args[1]);
+        //Примерные результаты тестирования:
+        //max_depth = 2, время = 1 сек, сайтов = 74, потоков 20
+        //max_depth = 3, время ~ 10 сек, сайтов ~ 2800, потоков 20
+        //max_depth = 4, время ~ 29.4 мин, сайтов ~ 130000, потоков 20
+        //String site = args[0];
+        //int max_depth = Integer.parseInt(args[1]);
+        String site = "http://bio.acousti.ca";
+        int max_depth = 3;
         Crawler crawler = new Crawler();
         long m = System.currentTimeMillis();
-        HashSet<URLDepthPair> res = crawler.getSites(site, max_depth);
-        System.out.println((double) (System.currentTimeMillis() - m));
-        print_list(res);
-    }
-    public static void print_list(HashSet<URLDepthPair> list) {
-        if(list == null) throw new IllegalArgumentException("List cannot be null");
-        for(URLDepthPair el : list)
-            System.out.println(el);
-        System.out.println(list.size());
+        HashSet<URLDepthPair> res = crawler.getSites(site, max_depth, 20);
+        System.out.println("За " + (double) (System.currentTimeMillis() - m) + " мс, найдено " + res.size() + " сайтов!");
+
+        for(URLDepthPair pair : res)
+            Logger.writeSites(pair.toString());
     }
 }
--- src/Crawler.java
+++ src/Crawler.java
@@ -1,4 +1,3 @@
-import java.util.ArrayDeque;
 import java.util.HashSet;
 
 public class Crawler {
@@ -7,94 +6,107 @@ public class Crawler {
     private static final String CLOSE_TAG = "</a>";
     private static final String PAR_HREF = "href=\"";
 
-    //Максимальная глубина
+    //Максимальная глубина поиска
     private int max_depth;
-    //Глубина в данный момент
-    private int curr_depth;
-    //Url сайта на котором происходит parse
-    private Url curr_url;
 
-    //Посещенные сайты
-    private final HashSet<URLDepthPair> visited;
-    //Не посещенные сайты
-    private final ArrayDeque<URLDepthPair> no_visited;
-
+    //Массив потоков
+    private CrawlerTask[] tasks;
     public Crawler() {
-        visited = new HashSet<>();
-        no_visited = new ArrayDeque<>();
+        tasks = null;
     }
 
-    //Функция принимает  страницу и добавляет
-    //все найденные ссылки в no_visited
-    private void parseUrls(StringBuilder page) {
-        int open = page.indexOf(OPEN_TAG);
-        int close = page.indexOf(CLOSE_TAG);
-        int href, close_href;
-        while(open != -1) {
-            href = page.indexOf(PAR_HREF, open + OPEN_TAG.length());
-            if(href != -1 && href < close && href > open) {
-                close_href = page.indexOf("\"", href + PAR_HREF.length());
-                addValidUrl(page.substring(href + PAR_HREF.length(), close_href));
+    //Класс задача
+    private class CrawlerTask extends Thread {
+        //Пул сайтов
+        private final URLPool pool;
+        //Рассматриваемый URLDepthPair
+        private URLDepthPair curr_pair;
+        //Статус потока - isFree = true - свободен
+        private boolean isFree;
+        public CrawlerTask(URLPool pool) {
+            this.pool = pool;
+            this.curr_pair = null;
+            this.isFree = true;
+        }
+
+        public synchronized void run() {
+            while (!isInterrupted()) {
+                //isFree = true;
+                //Берем сайт из пула
+                try { curr_pair = pool.poll(); }
+                catch (Exception e) { isFree = true; continue; }
+                //Если пул был не пустой, то добавляем сайт в массив посещеных сайтов
+                pool.push_visited(curr_pair);
+                //Если глубина < max_depth - 1, то парсим этот сайт
+                if(curr_pair.getDepth() < max_depth - 1) {
+                    isFree = false;
+                    StringBuilder page = Request.getPage(curr_pair.getUrl());
+                    if (page != null) parseUrls(page);
+                }
+                isFree = true;
             }
-            open = page.indexOf(OPEN_TAG, close + CLOSE_TAG.length());
-            close = page.indexOf(CLOSE_TAG, open + OPEN_TAG.length());
-        }
-    }
-
-    //Дополнительная функция, проверяющая валидной Url
-    //и добавление в no_visited
-    private void addValidUrl(String link) {
-        link = link.trim();
-        Url url;
-        if(link.startsWith("/"))
-            url = new Url(curr_url.getFullHost() + link);
-        else if(link.startsWith("http"))
-            url = new Url(link);
-        else return;
-        URLDepthPair udp = new URLDepthPair(url, curr_depth);
-        no_visited.add(udp);
-    }
-
-    //Функция рекурсивно берет сайт из
-    //no_visited парся его.
-    private void getSites() {
-        //Если достигли max_depth - 1, то заканчиваем парсинг
-        if(curr_depth == max_depth - 1){
-            while(!no_visited.isEmpty())
-                visited.add(no_visited.pollFirst());
-            return;
-        }
-        //Или если no_visited пустой
-        if(no_visited.size() == 0)
-            return;
-        StringBuilder page;
-        int size = no_visited.size();
-        curr_depth++;
-        for(int i = 0; i < size; i++) {
-            URLDepthPair el = no_visited.pollFirst();
-            if(visited.contains(el) || el == null) continue;
-            visited.add(el);
-            System.out.println("Current depth: " + curr_depth + " Index site: " + i + " Visited size: " + visited.size());
-            curr_url = el.getUrl();
-            page = Request.getPage(curr_url);
-            if(page != null) parseUrls(page);
         }
 
-        getSites();
+        //Функция принимает  страницу и добавляет
+        //все найденные ссылки в no_visited
+        private void parseUrls(StringBuilder page) {
+            int open = page.indexOf(OPEN_TAG);
+            int close = page.indexOf(CLOSE_TAG);
+            int href, close_href;
+            while(open != -1) {
+                href = page.indexOf(PAR_HREF, open + OPEN_TAG.length());
+                if(href != -1 && href < close && href > open) {
+                    close_href = page.indexOf("\"", href + PAR_HREF.length());
+                    addValidUrl(page.substring(href + PAR_HREF.length(), close_href));
+                }
+                open = page.indexOf(OPEN_TAG, close + CLOSE_TAG.length());
+                close = page.indexOf(CLOSE_TAG, open + OPEN_TAG.length());
+            }
+        }
+
+        //Дополнительная функция, проверяющая валидной Url
+        //и добавление в no_visited
+        private synchronized void addValidUrl(String link) {
+            link = link.trim();
+            Url url;
+            if(link.startsWith("/"))
+                url = new Url(curr_pair.getUrl().getFullHost() + link);
+            else if(link.startsWith("http"))
+                url = new Url(link);
+            else return;
+            URLDepthPair udp = new URLDepthPair(url, curr_pair.getDepth() + 1);
+            pool.push(udp);
+        }
     }
 
     //Принимает ссылку на первый сайт в формате protocol://host:port/path
     //и максимальную глубину для парсинга
-    public HashSet<URLDepthPair> getSites(String site, int _max_depth) {
+    public HashSet<URLDepthPair> getSites(String site, int _max_depth, int count_tasks) {
         if(_max_depth < 1)
             throw new IllegalArgumentException("Depth cannot be less than 1");
-        visited.clear();
-        no_visited.clear();
-        curr_depth = 0;
-        curr_url = new Url(site);
         max_depth = _max_depth;
-        addValidUrl(site);
-        getSites();
-        return visited;
+        tasks = new CrawlerTask[count_tasks];
+
+        URLPool pool = new URLPool();
+        pool.push(new URLDepthPair(new Url(site), 0));
+        //Запускаем поток
+        for(int i = 0; i < count_tasks; i++) {
+            tasks[i] = new CrawlerTask(pool);
+            tasks[i].start();
+        }
+        //Ожидаем когда все потоки освободятся и пул станет пустым
+        boolean isAllFree = true;
+        do {
+            try { Thread.sleep(1000); }
+            catch (Exception e) { continue; }
+            isAllFree = true;
+            for(int i = 0; i < count_tasks; i++)
+                isAllFree &= tasks[i].isFree;
+        } while(!isAllFree || !pool.isEmpty());
+        //Прерываем все потоки
+        for(int i = 0; i < count_tasks; i++)
+            tasks[i].interrupt();
+
+        return pool.getVisited();
     }
 }
